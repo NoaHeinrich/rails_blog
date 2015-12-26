231da06e9974091ea576462da6091aacc2bc28a1
Noah Heinrich <noahheinrich@gmail.com>
2015-12-26 17:58:11 -0600

    Tokens?

--- Desktop/Dev-Bootcamp/phase-3/blog/app/controllers/pages_controller.rb
+++ Desktop/Dev-Bootcamp/phase-3/blog/app/controllers/pages_controller.rb
@@ -3,13 +3,13 @@ class PagesController < ApplicationController
   end
 
   def login
-  #   user = User.find_by(email: params[:email])
-  #   if user && user.authenticate(params[:email], params[:password])
-  #     session[:user_id] = user.id
-  #     redirect_to "/"
-  #   else
-  #     @errors = "The credentials provided do not match"
-  #     redirect_to :back
-  # end
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:email], params[:password])
+      session[:user_id] = user.id
+      redirect_to "/"
+    else
+      @errors = "The credentials provided do not match"
+      # redirect_to :back
+    end
   end
 end
