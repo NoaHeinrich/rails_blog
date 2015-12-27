8df8524d9398bdd0a4ab1b8da42406057f5beb5d
Noah Heinrich <noahheinrich@gmail.com>
2015-12-27 10:10:08 -0600

    Add login and logout

--- Desktop/Dev-Bootcamp/phase-3/blog/app/controllers/pages_controller.rb
+++ Desktop/Dev-Bootcamp/phase-3/blog/app/controllers/pages_controller.rb
@@ -6,10 +6,16 @@ class PagesController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:email], params[:password])
       session[:user_id] = user.id
+      p session
       redirect_to "/"
     else
       @errors = "The credentials provided do not match"
       # redirect_to :back
     end
+  end
+
+  def logout
+    session.delete(:user_id)
+    redirect_to "/"
   end
 end
